93923d100e09be6c2a7a93ccfeca5c28c18e4e8a
Francisco Berti da Cruz <fbc991@live.com>
2023-07-17 13:27:40 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef, useState } from 'react';
+import { links } from '../data';
+import logo from '../logo.svg';
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const refNav = useRef(null);
+  const [height, setHeight] = useState('');
+
+  useEffect(() => {
+    refNav.current !== null &&
+      setHeight(refNav.current.getBoundingClientRect().bottom);
+  }, [showLinks]);
+
+  return (
+    <section
+      className="menu"
+      style={{
+        height: showLinks ? `${height}px` : '72px',
+      }}
+    >
+      <div>
+        <img src={logo} alt="logo" />
+        <button
+          type="button"
+          onClick={() => {
+            setShowLinks(!showLinks);
+          }}
+        >
+          <span></span> <span></span> <span></span>
+        </button>
+      </div>
+      {showLinks && (
+        <nav ref={refNav}>
+          <ul>
+            {links.map(({ id, text, url }) => (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
+    </section>
+  );
+};
+
+export default Navbar;
